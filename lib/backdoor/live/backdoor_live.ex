b5c8f2bea60f44aa93eea47c4029f6cb6f1a74c5
Hubert Łępicki <hubert.lepicki@amberbit.com>
2020-07-29 10:02:16 +0200

    Added very naive executor

--- lib/backdoor/live/backdoor_live.ex
+++ lib/backdoor/live/backdoor_live.ex
@@ -7,7 +7,7 @@ defmodule Backdoor.BackdoorLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket |> assign(bindings: [], output: "")}
+    {:ok, socket |> assign(bindings: [], output: [])}
   end
 
   @impl true
@@ -26,9 +26,9 @@ defmodule Backdoor.BackdoorLive do
         </div>
         <!-- Output -->
         <div class="px-6 py-4 flex-1 overflow-y-scroll">
-          <%= for i <- (1..100) do %>
+          <%= for message <- @output do %>
             <div class="flex items-start text-sm">
-              <pre>This is a text message</pre>
+              <pre><%= message %></pre>
             </div>
           <% end %>
         </div>
@@ -38,11 +38,21 @@ defmodule Backdoor.BackdoorLive do
             <span class="text-xl text-grey border-r-0 border-grey p-2">
               iex&gt;
             </span>
-            <input type="text" class="w-full px-4" placeholder="Message #general" />
+            <%= f = form_tag "#", [phx_submit: :execute, class: "flex w-full"] %>
+              <%= text_input :command, :text, [placeholer: "Write Elixir code to execute and hit 'Enter'...", value: "", class: "w-full px-4 outline-none"] %>
+            </form>
           </div>
         </div>
       </div>
     </div>
     """
+  end
+
+  def handle_event("execute", %{"command" => %{"text" => command}}, socket) do
+    {:ok, ast} = Code.string_to_quoted(command)
+    {result, bindings} = Code.eval_quoted(ast, socket.assigns.bindings, [])
+
+    output = socket.assigns.output ++ ["iex> " <> command] ++ [inspect(result)]
+    {:noreply, socket |> push_event("command", %{text: ""}) |> assign(bindings: bindings, output: output)}
   end
 end
