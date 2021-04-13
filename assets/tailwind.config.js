794a1538abf101e039650a564311edbd38c85b6a
Hubert Łępicki <hubert.lepicki@amberbit.com>
2021-04-13 21:57:07 +0200

    Configure Tailwind v2.0

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   variants: {},
   plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
   ]
 };
